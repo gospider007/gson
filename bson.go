1f714a0c27cc8af90c7c7de6d6f8ae617d020725
bxd <2216403312@qq.com>
2023-09-26 13:53:31 +0800

    first

---
+++ bson.go
@@ -0,0 +1,115 @@
+package bson
+
+import (
+	"errors"
+
+	"gitee.com/baixudong/tools"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/tidwall/gjson"
+	bbson "go.mongodb.org/mongo-driver/bson"
+)
+
+var jsonConfig = jsoniter.Config{
+	EscapeHTML:    true,
+	CaseSensitive: true,
+}.Froze()
+
+type Client struct {
+	g gjson.Result
+}
+
+func (obj *Client) Map() map[string]*Client {
+	result := map[string]*Client{}
+	for kk, vv := range obj.g.Map() {
+		result[kk] = &Client{g: vv}
+	}
+	return result
+}
+func (obj *Client) Array() []*Client {
+	lls := obj.g.Array()
+	result := make([]*Client, len(lls))
+	for i := 0; i < len(lls); i++ {
+		result[i] = &Client{g: lls[i]}
+	}
+	return result
+}
+func (obj *Client) Value() any {
+	return obj.g.Value()
+}
+func (obj *Client) String() string {
+	return obj.g.String()
+}
+func (obj *Client) Bytes() []byte {
+	return tools.StringToBytes(obj.g.Raw)
+}
+func (obj *Client) Int() int64 {
+	return obj.g.Int()
+}
+func (obj *Client) Raw() string {
+	return obj.g.Raw
+}
+func (obj *Client) Bool() bool {
+	return obj.g.Bool()
+}
+func (obj *Client) Exists() bool {
+	return obj.g.Exists()
+}
+func (obj *Client) IsArray() bool {
+	return obj.g.IsArray()
+}
+func (obj *Client) IsObject() bool {
+	return obj.g.IsObject()
+}
+func (obj *Client) Float() float64 {
+	return obj.g.Float()
+}
+func (obj *Client) Uint() uint64 {
+	return obj.g.Uint()
+}
+func (obj *Client) Get(path string) *Client {
+	return &Client{g: obj.g.Get(path)}
+}
+func (obj *Client) MarshalJSON() ([]byte, error) {
+	return Encode(obj.g.Value())
+}
+func (obj *Client) MarshalBSON() ([]byte, error) {
+	return bbson.Marshal(obj.g.Value())
+}
+func Encode(data any) ([]byte, error) {
+	return jsonConfig.Marshal(data)
+}
+
+// 转成struct
+func Decode(data any, strus ...any) (client *Client, err error) {
+	var ga gjson.Result
+	switch val := data.(type) {
+	case gjson.Result:
+		ga = val
+	case []byte:
+		if len(strus) > 0 {
+			err = jsonConfig.Unmarshal(val, strus[0])
+		}
+		ga = gjson.ParseBytes(val)
+	case string:
+		if len(strus) > 0 {
+			err = jsonConfig.Unmarshal(tools.StringToBytes(val), strus[0])
+		}
+		ga = gjson.Parse(val)
+	default:
+		con, err := Encode(data)
+		if err != nil {
+			return nil, err
+		}
+		if len(strus) > 0 {
+			err = jsonConfig.Unmarshal(con, strus[0])
+		}
+		ga = gjson.ParseBytes(con)
+	}
+	if err == nil {
+		if !ga.IsObject() && !ga.IsArray() {
+			err = errors.New("不是一个json对象")
+		}
+	}
+	client = &Client{g: ga}
+	return
+}
---
+++ go.mod
@@ -0,0 +1,25 @@
+module gitee.com/baixudong/bson
+
+go 1.21.1
+
+require (
+	gitee.com/baixudong/tools v0.0.0-20230926010219-77f29cbf5935
+	github.com/json-iterator/go v1.1.12
+	github.com/tidwall/gjson v1.17.0
+	go.mongodb.org/mongo-driver v1.12.1
+)
+
+require (
+	gitee.com/baixudong/kinds v0.0.0-20230809033013-c3d6d3479f8c // indirect
+	gitee.com/baixudong/re v0.0.0-20230809033040-360c1d945e59 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	golang.org/x/image v0.12.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
